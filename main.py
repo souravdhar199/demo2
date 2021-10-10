a0f4c69b31cb87dbacddfdbc73db085ddea9cc0c
sourav199 <souravdhar1999@gmail.com>
2021-10-10 10:43:20 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import flask
+import os
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return flask.render_template('index.html')
+
+
+if __name__ =="__main__":
+  app.run(
+    host=os.getenv("IP", "0.0.0.0"),
+    port=int(os.getenv("PORT", "8080")),
+)
